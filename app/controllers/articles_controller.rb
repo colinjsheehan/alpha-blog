34caf41913a7ed071019be001ac95aece970cd11
colinsheehan <colin.sheehan@ucdconnect.ie>
2018-11-10 09:18:50 +0000

    Article editing functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,17 @@
 class ArticlesController < ApplicationController
+    
+    def index 
+        # grab all articles
+        @articles = Article.all
+    end
+    
     def new
         @article = Article.new 
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end 
 
     def create
         @article = Article.new(article_params)
@@ -13,11 +23,20 @@ class ArticlesController < ApplicationController
              render 'new'
         end
     end
-    ######################################################  
+
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
-    ######################################################
     
     private 
     def article_params
