58a2c2691dc9125225e25a78d50eea854e6f2f57
colinsheehan <colin.sheehan@ucdconnect.ie>
2018-11-10 12:25:38 +0000

    remove some redundent code & send for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+   
+    before_action :set_article, only: [:edit, :update, :show, :destroy] # add this 
+    
     
     def index 
         # grab all articles
@@ -10,7 +13,7 @@ class ArticlesController < ApplicationController
     end
     # from the articles find article by ID
     def edit
-        @article = Article.find(params[:id])
+      
     end 
 
     def create
@@ -25,7 +28,7 @@ class ArticlesController < ApplicationController
     end
 
     def update 
-        @article = Article.find(params[:id])
+       
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -35,12 +38,12 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id])
+        
     end
     
     
     def destroy
-        @article = Article.find(params[:id])
+
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
@@ -48,10 +51,29 @@ class ArticlesController < ApplicationController
     
     
     private 
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
+    
+        def set_article
+            @article = Article.find(params[:id])
+        end 
+        
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
