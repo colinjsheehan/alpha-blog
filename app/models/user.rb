3c7691cb6d54728ab8ac764e28ca06790b6c468a
colinsheehan <colin.sheehan@ucdconnect.ie>
2018-11-11 17:59:35 +0000

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
-class User < ActiveRecord:: Base
-    
+class User < ActiveRecord::Base
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+             uniqueness: { case_sensitive: false },
+             format: { with: VALID_EMAIL_REGEX }
 end
+
+
+# class User < ActiveRecord::Base
+#   has_many :articles, dependent: :destroy
+#   before_save { self.email = email.downcase }
+  
+#   validates :username, presence: true, 
+#             uniqueness: { case_sensitive: false }, 
+#             length: { minimum: 3, maximum: 25 }
+            
+#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#   validates :email, presence: true, length: { maximum: 105 },
+#             uniqueness: { case_sensitive: false },
+#             format: { with: VALID_EMAIL_REGEX }
+#   has_secure_password
+# end
